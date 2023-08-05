fea2cd97e38a2828f163a5b4760552601ee9b7d5
macha <chaimamahdadi1999@gmail.com>
2023-08-05 06:55:13 +0100

    initial commit3

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,146 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:typed_data';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:image/image.dart' as img;
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Sign Language App',
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Interpreter? interpreter;
+  ImagePicker picker = ImagePicker();
+  String prediction = '';
+  List<String> labels = [];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    loadModel();
+    loadLabels();
+  }
+
+  // Load the TFLite model
+  Future<void> loadModel() async {
+    try {
+      String modelPath = 'assets/sign_language_model.tflite';
+      interpreter = await Interpreter.fromAsset(modelPath);
+      print('Model loaded successfully');
+    } catch (e) {
+      print('Error loading model: $e');
+    }
+  }
+
+  // Load the labels from the labels.txt file in the assets
+  Future<void> loadLabels() async {
+    try {
+      String labelsText = await rootBundle.loadString('assets/labels.txt');
+      labels = labelsText.trim().split('\n');
+      print('Labels loaded successfully');
+    } catch (e) {
+      print('Error loading labels: $e');
+    }
+  }
+
+  // Run inference on the image
+  Future<void> runInference(File imageFile) async {
+    if (interpreter == null) {
+      print('Model is not loaded yet.');
+      return;
+    }
+
+    try {
+      var imageBytes = await imageFile.readAsBytes();
+      img.Image image = img.decodeImage(imageBytes)!;
+
+      // Resize the image to 64x64
+      image = img.copyResize(image, width: 64, height: 64);
+
+      var input = imageToByteListFloat32(
+          image, 64, 255.0); // Convert image to model input format
+
+      // Perform inference
+      var output = Float32List(
+          interpreter!.getOutputTensor(0).shape.reduce((a, b) => a * b));
+      interpreter!.run(input, output);
+
+      // Process the output and get the prediction
+      int maxIndex = output.indexOf(output.reduce((a, b) => a > b ? a : b));
+      String result = labels[maxIndex];
+      setState(() {
+        prediction = result;
+      });
+    } catch (e) {
+      print('Error running inference: $e');
+    }
+  }
+
+  // Image picker function
+  Future<void> getImageAndRunInference() async {
+    XFile? pickedImage = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedImage != null) {
+      runInference(File(pickedImage.path));
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Sign Language App'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            ElevatedButton(
+              onPressed: getImageAndRunInference,
+              child: Text('Select Image'),
             ),
+            SizedBox(height: 20),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              prediction.isEmpty
+                  ? 'Select an image to make a prediction'
+                  : 'Prediction: $prediction',
+              style: TextStyle(fontSize: 20),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+// Image processing helper function
+Uint8List imageToByteListFloat32(
+    img.Image image, int inputSize, double normalization) {
+  var convertedBytes = Uint8List(1 * inputSize * inputSize);
+  var buffer = Float32List.view(convertedBytes.buffer);
+  var pixelIndex = 0;
+  for (var i = 0; i < inputSize; i++) {
+    for (var j = 0; j < inputSize; j++) {
+      var pixel = image.getPixel(j, i);
+      var grayscaleValue = img.getLuminance(pixel).round();
+      buffer[pixelIndex] = grayscaleValue / normalization;
+      pixelIndex++;
+    }
+  }
+  return convertedBytes;
 }
