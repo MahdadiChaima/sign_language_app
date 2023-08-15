a82f9f73a7f57ad76035983ec2593de7b1f3c72c
macha <chaimamahdadi1999@gmail.com>
2023-08-15 20:44:45 +0100

    commit 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:tflite/tflite.dart';
 
 void main() => runApp(MyApp());
 
@@ -27,50 +23,6 @@ class TfliteModel extends StatefulWidget {
 }
 
 class _TfliteModelState extends State<TfliteModel> {
-  late File _image;
-  late List _results;
-  bool _imageSelect = false;
-
-  @override
-  void initState() {
-    super.initState();
-    loadModel();
-  }
-
-  Future loadModel() async {
-    String? res = await Tflite.loadModel(
-      model: 'assets/sign_language_model.tflite',
-      labels: 'assets/labels.txt',
-    );
-    print('Model loading status: $res');
-  }
-
-  Future imageClassification(File image) async {
-    final List? recognitions = await Tflite.runModelOnImage(
-      path: image.path,
-      numResults: 6,
-      threshold: 0.05,
-      imageMean: 127.5,
-      imageStd: 127.5,
-    );
-    setState(() {
-      _results = recognitions!;
-      _image = image;
-      _imageSelect = true;
-    });
-  }
-
-  Future pickImage() async {
-    final ImagePicker _picker = ImagePicker();
-    final XFile? pickedFile = await _picker.pickImage(
-      source: ImageSource.gallery,
-    );
-    if (pickedFile != null) {
-      File image = File(pickedFile.path);
-      imageClassification(image);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -78,39 +30,7 @@ class _TfliteModelState extends State<TfliteModel> {
         title: Text('Sign Language App'),
       ),
       body: Column(
-        children: [
-          GestureDetector(
-            onTap: () => pickImage(),
-            child: Container(
-              margin: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                border: Border.all(),
-              ),
-              child: _imageSelect
-                  ? Image.file(_image, fit: BoxFit.cover)
-                  : Center(child: Text('No image selected')),
-            ),
-          ),
-          SingleChildScrollView(
-            child: Column(
-              children: _imageSelect
-                  ? _results.map((result) {
-                      return Card(
-                        child: ListTile(
-                          title: Text(
-                            '${result['label']} - ${result['confidence'].toStringAsFixed(2)}',
-                            style: TextStyle(
-                              color: Colors.red,
-                              fontSize: 18,
-                            ),
-                          ),
-                        ),
-                      );
-                    }).toList()
-                  : [],
-            ),
-          ),
-        ],
+        children: [],
       ),
     );
   }
