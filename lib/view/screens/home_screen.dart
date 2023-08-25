ee215a45042b4339b7a9f18adbce34d0f5ad1c8b
macha <chaimamahdadi1999@gmail.com>
2023-08-25 16:06:31 +0100

    app2

--- lib/view/screens/home_screen.dart
+++ lib/view/screens/home_screen.dart
@@ -17,12 +17,42 @@ class _HomeScreenState extends State<HomeScreen> {
   bool isAlphabetMode = true;
   final FlutterTts flutterTts = FlutterTts();
   final List<String> letters = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-    'k', 'l', 'm', 'n', 'o',
-    'p', 'q', 'r', 's', 't',
-    'u', 'v', 'w', 'x', 'y', 'z', ' ', '@',
-    '.', // Add space and point characters
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9'
   ];
 
   @override
